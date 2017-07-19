eb731dc06360d2a5d8d2920d45b93ba864e5c0ea
sunchenglu <sunchenglu@ichunqiu.com>
2017-07-19 12:05:25 +0800

    q3

---
+++ src/3.Longest_Substring_Without_Repeating_Characters.go
@@ -0,0 +1,34 @@
+package src
+
+func lengthOfLongestSubstring(s string) int {
+
+	length := len(s)
+
+	if length < 2 {
+		return length
+	}
+
+	start := 0
+	longest := 0
+
+	chars := make(map[string]int)
+
+	for i, char := range s {
+
+		num, exist := chars[string(char)]
+
+		if exist && start <= num {
+			start = num + 1
+		} else{
+			if longest > (i+1-start) {
+				longest = longest
+			}else {
+				longest = i+1-start
+			}
+		}
+
+		chars[string(char)] = i
+	}
+
+	return longest
+}
